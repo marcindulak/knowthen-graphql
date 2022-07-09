2dd7bf8cd8ffdbb9a72f552a0d8cd195fe63c67e
Marcin Dulak <Marcin.Dulak@gmail.com>
2022-07-09 19:02:07 +0200

    Update hackerbookreview_1/web

--- hackerbookreview_1/web/src/AddBook.js
+++ hackerbookreview_1/web/src/AddBook.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { BookSearchForm, BookSearchResults } from './components/Book';
 import Error from './components/Error';
 import data from './data';
@@ -41,7 +41,7 @@ class AddBook extends Component {
     const { redirectBookId } = this.state;
     return (
       <div className="cf black-80 mv2">
-        {redirectBookId && <Redirect to={`/book/${redirectBookId}`} />}
+        {redirectBookId && <Navigate to={`/book/${redirectBookId}`} />}
         <Error errors={this.state.errors} />
         <BookSearchForm
           search={this.search}
