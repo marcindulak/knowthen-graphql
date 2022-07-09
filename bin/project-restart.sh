1fca1926e23ab2c5be3d3e3367a892842add136e
Marcin Dulak <Marcin.Dulak@gmail.com>
2022-07-09 19:01:33 +0200

    Use postgres user for psql

--- bin/project-restart.sh
+++ bin/project-restart.sh
@@ -22,4 +22,4 @@ docker-compose -f docker-compose.yml build --build-arg USER_UID=$(id -u) --build
 docker-compose -f docker-compose.yml up -d database server web
 # https://github.com/docker/compose/issues/7306#issuecomment-789667409
 # https://stackoverflow.com/questions/35069027/docker-wait-for-postgresql-to-be-running/39028690#39028690
-echo "exec server: npm run initdb" && docker-compose -f docker-compose.yml exec -T server sh -c "until pg_isready -h database; do sleep 1; done && npm run initdb"
+echo "exec server: npm run initdb" && docker-compose -f docker-compose.yml exec -T server sh -c "until pg_isready -h database -U postgres; do sleep 1; done && npm run initdb"
