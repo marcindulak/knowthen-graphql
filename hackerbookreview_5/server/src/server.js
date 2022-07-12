4c4f6df858483985a2b142266d2daec626c01563
Marcin Dulak <Marcin.Dulak@gmail.com>
2022-07-12 21:17:09 +0200

    Update hackerbookreview_5/server/src/server.js

--- hackerbookreview_5/server/src/server.js
+++ hackerbookreview_5/server/src/server.js
@@ -1,31 +1,29 @@
 import express from 'express';
-import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import { makeExecutableSchema } from 'graphql-tools';
-import { graphql } from 'graphql';
+import { ApolloServer }  from 'apollo-server-express';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+import { makeExecutableSchema } from '@graphql-tools/schema';
 import typeDefs from './typedefs';
 import resolvers from './resolvers';
 import loaders from './loader';
+
+const PORT = 4000;
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 const app = express();
 
-app.use(cors());
-
-app.use('/graphql', 
-  bodyParser.json(), 
-  graphqlExpress(() => ({ 
-    schema,
-    context: {
-      loaders: loaders()
-    }
-  }))
-);
-
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-
-app.listen(4000, () => {
-  console.log('Go to http://localhost:4000/graphiql to run queries!');
+const server = new ApolloServer({
+  schema,
+  plugins: [
+    ApolloServerPluginLandingPageGraphQLPlayground(),
+  ],
+  context: {
+    loaders: loaders()
+  },
 });
+await server.start();
+
+server.applyMiddleware({ app, path: '/graphql', cors: true });
+
+await new Promise(resolve => app.listen({ port: PORT }, resolve));
+console.log(`Go to http://localhost:${PORT}${server.graphqlPath} to run queries!`);
